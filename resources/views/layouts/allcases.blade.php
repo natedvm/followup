22087da74a016a6e6c08763fdfda0bb1b13e89ef
Nate Miller <natedvm@gmail.com>
2017-12-18 22:08:12 -0700

    Formatted directory - directed main route to the all cases view in layouts

---
+++ resources/views/layouts/allcases.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('title','Cases')
+
+@section('content')
+    Test content
+@endsection
---
+++ resources/views/main.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+    Main Page
+@endsection
+
+@section('content')
+    <div class="container">
+        <p>This is the main page of my followup project</p>
+    </div>
+@endsection
