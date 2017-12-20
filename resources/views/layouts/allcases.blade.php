2a0d706064a6765ba9fd0812cf4752c259fac33e
Nate Miller <natedvm@gmail.com>
2017-12-19 17:22:18 -0700

    Started putting in layouts.  Added Nav bar and bootstrap style file link on the master layout

--- resources/views/layouts/allcases.blade.php
+++ resources/views/layouts/allcases.blade.php
@@ -3,5 +3,5 @@
 @section('title','Cases')
 
 @section('content')
-    Test content
+
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,11 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <title> @yield('title')</title>
 </head>
 
 <body>
+@include('partials.navbar')
 @yield('content')
 </body>
 </html>
