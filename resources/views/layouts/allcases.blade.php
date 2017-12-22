5ec1de44820df5123b433efc25757407ef0fee5b
Nate Miller <natedvm@gmail.com>
2017-12-22 06:28:34 -0700

    Started to try to make layout for the table creation in allcases layout

--- resources/views/layouts/allcases.blade.php
+++ resources/views/layouts/allcases.blade.php
@@ -3,5 +3,25 @@
 @section('title','Cases')
 
 @section('content')
+    <div class="container">
+        <table id="procedure_table">
+            <thead>
+            <tr>
+                @foreach($procedures[0] as $header => $data)
+                    {{$header}}
+                @endforeach
+            </tr>
+            </thead>
 
+            <tbody>
+            @foreach($procedures as $procedure)
+                @foreach($procedure as $data)
+                    <td>
+                        {{$data}}
+                    </td>
+                @endforeach
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
