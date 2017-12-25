c2377a05421ef0eb86d1b8ab1f603cc481b0fbd6
Nate Miller <natedvm@gmail.com>
2017-12-25 14:09:13 -0700

    Added datatables and enabled table on allcases page

--- resources/views/layouts/allcases.blade.php
+++ resources/views/layouts/allcases.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.master')
 
+@section('pagescript')
+    <script type="text/javascript" src="{{asset('js/layouts/allcases.js')}}"></script>
+@endsection
+
 @section('title','Cases')
 
 @section('content')
 
     <div class="container-fluid pull-left">
-        <table class="table table-bordered table-hover">
+        <table id="procedureTable" class="table table-bordered table-hover">
             <thead>
             <tr>
                 @foreach($procedures->first()->toArray() as $header => $data)
