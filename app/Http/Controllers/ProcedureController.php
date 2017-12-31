1f8b9f1b3f2c9edd3a8ba0f7cea6989d3cf60352
Nate Miller <natedvm@gmail.com>
2017-12-31 16:48:26 -0700

    Add case form now working - export field not yet hooked up. Return user to all cases - and put alert up that case added successfully

--- app/Http/Controllers/ProcedureController.php
+++ app/Http/Controllers/ProcedureController.php
@@ -20,8 +20,15 @@ class ProcedureController extends Controller
     public function postAddProcedure(Request $request){
         $procedure = new Procedure();
         $procedure->patient_name = $request->input('patient_name');
-        \Debugbar::info($procedure);
+        $procedure->last_name = $request->input('last_name');
+        $procedure->dvmax_id = $request->input('dvmax_id');
+        $procedure->date_of_birth = $request->input('date_of_birth');
+        $procedure->sex = $request->input('sex');
+        $procedure->breed = $request->input('breed');
+        $procedure->weight = $request->input('weight');
+        $procedure->email = $request->input('email');
         $procedure->save();
-        return redirect()->route('layouts.allcases')->with('info','Show this info');
+        return redirect()->route('layouts.allcases')->with('info', $procedure->patient_name . " " .
+    $procedure->last_name . " was added successfully");
     }
 }
