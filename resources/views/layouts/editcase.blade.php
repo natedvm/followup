6ee29f75e2ee6218f784a9211e751cdcd7c7361e
Nate Miller <natedvm@gmail.com>
2018-01-04 06:10:14 -0700

    Entered fields in editCase form

--- resources/views/layouts/editcase.blade.php
+++ resources/views/layouts/editcase.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 
     <div class="container-fluid">
-{{--Hidden alert to display by jquery when chanages aren't yet saved.--}}
+        {{--Hidden alert to display by jquery when chanages aren't yet saved.--}}
         <div class="row">
             <div class="col-md-12">
                 <p class="alert alert-info hidden">Un-saved changes are present</p>
@@ -40,38 +40,123 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4">
-
+            <div class="form-group col-md-4">
+                <label class="control-label" for="date_of_birth">Date of Birth</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="date_of_birth" name="date_of_birth">
+                </div>
             </div>
-            <div class="col-md-4">
-
+            <div class="form-group col-md-4">
+                <label class="control-label" for="sex">Sex</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="sex" name="sex">
+                </div>
             </div>
-            <div class="col-md-4">
-
+            <div class="form-group col-md-4">
+                <label class="control-label" for="breed">Breed</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="breed" name="breed">
+                </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4">
-
+            <div class="form-group col-md-4">
+                <label class="control-label" for="weight">Weight</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="weight" name="weight">
+                </div>
             </div>
-            <div class="col-md-4">
-
+            <div class="form-group col-md-4">
+                <label class="control-label" for="email">Email</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="email" name="email">
+                </div>
             </div>
-            <div class="col-md-4">
-
+            <div class="form-group col-md-4">
+                <label class="control-label" for="date_of_surgery">Date of Surgery</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="date_of_surgery" name="date_of_surgery">
+                </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4">
-
+            <div class="form-group col-md-4">
+                <label class="control-label" for="side">Side</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="side" name="side">
+                </div>
             </div>
-            <div class="col-md-4">
-
+            <div class="form-group col-md-4">
+                <label class="control-label" for="preop_angle">Preop Angle</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="preop_angle" name="preop_angle">
+                </div>
             </div>
-            <div class="col-md-4">
-
+            <div class="form-group col-md-4">
+                <label class="control-label" for="blade_size">Blade Size</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="blade_size" name="blade_size">
+                </div>
             </div>
         </div>
+        <div class="row">
+            <div class="form-group col-md-4">
+                <label class="control-label" for="rotation">Rotation</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="rotation" name="rotation">
+                </div>
+            </div>
+            <div class="form-group col-md-4">
+                <label class="control-label" for="postop_angle">Postop Angle</label>
+                <div>
+                    <input type="number" class="form-control"
+                           id="postop_angle" name="postop_angle">
+                </div>
+            </div>
+            <div class="form-group col-md-4">
+                <label class="control-label" for="plate">Plate</label>
+                <div>
+                    <input type="text" class="form-control"
+                           id="plate" name="plate">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group col-md-4">
+                <label class="control-label" for="meniscal_status">Meniscal Status</label>
+                <div>
+                    <textarea class="form-control"
+                              id="meniscal_status" name="meniscal_status"></textarea>
+                </div>
+            </div>
+            <div class="form-group col-md-4">
+                <label class="control-label" for="comments">Comments</label>
+                <div>
+                    <textarea class="form-control"
+                              id="comments" name="comments"></textarea>
+                </div>
+            </div>
+            <div class="form-group col-md-4">
+                <label class="control-label" for="complications">Complications</label>
+                <div>
+                    <textarea class="form-control"
+                              id="complications" name="complications"></textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     </div>
 
 @endsection
