975e4fb4b5e5852a8d8a2d3b5f15aa12ec7478e3
Nate Miller <natedvm@gmail.com>
2018-01-07 18:38:33 -0700

    added edit nav bar button which actives when on editpage

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,13 +1,16 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">Case follow up</a>
+            <a class="navbar-brand" href="{{route('layouts.allcases')}}">Case follow up</a>
         </div>
         <ul class="nav navbar-nav">
             <li class=" @if( Request::path() == '/' ) active @endif ">
                 <a href="{{route('layouts.allcases')}}">View Cases</a></li>
             <li class=" @if( Request::path() == 'addcase' ) active @endif ">
                 <a href="{{route('layouts.addcase')}}">Add Case</a></li>
+            <li class=" @if( strstr(Request::path(),'editcase') ) active @endif ">
+                <a>Edit Case</a>
+            </li>
         </ul>
     </div>
 </nav>
