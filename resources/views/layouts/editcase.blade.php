bea758078280142da110788da5f3e9e073b37e86
Nate Miller <natedvm@gmail.com>
2018-01-11 14:13:57 -0700

    Edit case view values completed

--- resources/views/layouts/editcase.blade.php
+++ resources/views/layouts/editcase.blade.php
@@ -35,7 +35,7 @@
                 <label class="control-label" for="dvmax_id">DVMax ID</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="dvmax_id" name="dvmax_id">
+                           id="dvmax_id" name="dvmax_id" value="{{$procedure->dvmax_id}}">
                 </div>
             </div>
         </div>
@@ -44,21 +44,21 @@
                 <label class="control-label" for="date_of_birth">Date of Birth</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="date_of_birth" name="date_of_birth">
+                           id="date_of_birth" name="date_of_birth" value="{{$procedure->date_of_birth}}">
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="sex">Sex</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="sex" name="sex">
+                           id="sex" name="sex" value="{{$procedure->sex}}">
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="breed">Breed</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="breed" name="breed">
+                           id="breed" name="breed" value="{{$procedure->breed}}">
                 </div>
             </div>
         </div>
@@ -67,21 +67,21 @@
                 <label class="control-label" for="weight">Weight</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="weight" name="weight">
+                           id="weight" name="weight" value="{{$procedure->weight}}">
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="email">Email</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="email" name="email">
+                           id="email" name="email" value="{{$procedure->email}}">
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="date_of_surgery">Date of Surgery</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="date_of_surgery" name="date_of_surgery">
+                           id="date_of_surgery" name="date_of_surgery" value="{{$procedure->date_of_surgery}}">
                 </div>
             </div>
         </div>
@@ -90,21 +90,21 @@
                 <label class="control-label" for="side">Side</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="side" name="side">
+                           id="side" name="side" value="{{$procedure->side}}">
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="preop_angle">Preop Angle</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="preop_angle" name="preop_angle">
+                           id="preop_angle" name="preop_angle" value="{{$procedure->preop_angle}}">
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="blade_size">Blade Size</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="blade_size" name="blade_size">
+                           id="blade_size" name="blade_size" value="{{$procedure->blade_size}}">
                 </div>
             </div>
         </div>
@@ -113,21 +113,21 @@
                 <label class="control-label" for="rotation">Rotation</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="rotation" name="rotation">
+                           id="rotation" name="rotation" value="{{$procedure->rotation}}">
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="postop_angle">Postop Angle</label>
                 <div>
                     <input type="number" class="form-control"
-                           id="postop_angle" name="postop_angle">
+                           id="postop_angle" name="postop_angle" value="{{$procedure->postop_angle}}">
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="plate">Plate</label>
                 <div>
                     <input type="text" class="form-control"
-                           id="plate" name="plate">
+                           id="plate" name="plate" value="{{$procedure->plate}}">
                 </div>
             </div>
         </div>
@@ -136,27 +136,24 @@
                 <label class="control-label" for="meniscal_status">Meniscal Status</label>
                 <div>
                     <textarea class="form-control"
-                              id="meniscal_status" name="meniscal_status"></textarea>
+                              id="meniscal_status" name="meniscal_status">{{$procedure->meniscal_status}}</textarea>
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="comments">Comments</label>
                 <div>
                     <textarea class="form-control"
-                              id="comments" name="comments"></textarea>
+                              id="comments" name="comments">{{$procedure->comments}}</textarea>
                 </div>
             </div>
             <div class="form-group col-md-4">
                 <label class="control-label" for="complications">Complications</label>
                 <div>
                     <textarea class="form-control"
-                              id="complications" name="complications"></textarea>
+                              id="complications" name="complications">{{$procedure->complications}}</textarea>
                 </div>
             </div>
         </div>
-    </div>
-
-
     </div>
 
 @endsection
